dc316cf640a9fa7307dddfaf4c9fca87f36af680
theenghrajan <ss.dev.n99@gmail.com>
2024-06-26 16:25:42 +0545

    10: first commit

---
+++ custom/custom-template-lp.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Template Name: Custom Landing Page
+ *
+ */
+include 'header-custom-landing.php'; ?>
+
+<section class="container">
+	<h1>This is Heading One</h1>
+	<h2>This is Heading Two</h2>
+	<h3>This is Heading Three</h3>
+	<h4>This is Heading Four</h4>
+	<h5>This is Heading Five</h5>
+	<h6>This is Heading Six</h6>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error necessitatibus quibusdam illum ut explicabo repudiandae id mollitia ipsa, maiores aliquam, quidem, voluptatem tenetur debitis voluptatum vitae <a href="#">rerum quas voluptates iusto</a>.</p>
+	<a href="#" class="btn btn-primary">Button</a>
+</section>
+
+<?php
+include 'footer-custom-landing.php';
---
+++ custom/footer-custom-landing.php
@@ -0,0 +1,15 @@
+</main>
+
+<footer class="site-footer">
+  <div class="container">
+    <h2>This is footer.</h2>
+  </div>
+</footer>
+<!-- /.site-footer -->
+
+</div> <!-- /.site-wrapper -->
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
